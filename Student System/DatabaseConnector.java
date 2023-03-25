8159b3e91eb6607bc9f46aea9741d9661f379457
Victor M <97889760+Vic32675@users.noreply.github.com>
2023-03-25 18:28:44 +0300

    Update DatabaseConnector.java

--- Student System/DatabaseConnector.java
+++ Student System/DatabaseConnector.java
@@ -1,13 +1,44 @@
 import java.sql.Connection;
-import java.sql.*;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 import java.sql.SQLException;
-public class DatabaseConnector {
-    private static final String URL = "jdbc:mariadb://127.0.0.1:3306/studentsystem";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Ud415zAs";
+//import java.lang.ClassNotFoundException; //This is not necessary since java.lang is automatically imported on all java programs.
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+public class JDBC {
+    
+    final static String DRIVER = "org.mariadb.jdbc.driver";
+    final static String DBURL = "jdbc:mariadb://localhost:3306/task2";
+    final static String USER="root";
+    final static String KEY = "password";
+    
+    
+    //method to connect to the database
+    public Connection connection(){
+        
+        try{
+            Class.forName(DRIVER);
+            Connection conn = DriverManager.getConnection(DBURL,USER,KEY);
+            Statement stmt = conn.createStatement(); 
+            
+     
+            return conn;
+        }
+        
+        catch(SQLException | ClassNotFoundException e){
+            JOptionPane.showMessageDialog(null,e);
+            return null;
+        }
     }
+        
+
+
+
+public void exeQuery(String query){
+    connection();
+    ResultSet rst= stmt.executeQuery(query);
+    
+}
+    
 }
