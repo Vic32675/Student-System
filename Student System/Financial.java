d17d85b4874e83f445d0185a116461b6ecd97b31
Victor M <97889760+Vic32675@users.noreply.github.com>
2023-03-26 12:18:13 +0300

    Update Financial.java

--- Student System/Financial.java
+++ Student System/Financial.java
@@ -6,7 +6,7 @@ public class Financial extends JFrame {
     private JButton balanceButton;
     private JButton hostelsButton;
 
-    public Financial(){
+    public Financial() {
         setTitle("Financials");
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
@@ -16,20 +16,23 @@ public class Financial extends JFrame {
         feeStructureButton = new JButton("Fee Structure");
         //flex code
         financialPanel.add(feeStructureButton);
-//        feeStructureButton.addActionListener(e -> {
-//            FeeStructure feeStructurewindow = new FeeStructure();
-//            feeStructurewindow.setVisible(true);
-//        });
+        feeStructureButton.addActionListener(e -> {
+            FeeStructure feeStructurewindow = new FeeStructure();
+            feeStructurewindow.setVisible(true);
+        });
 
         balanceButton = new JButton("Fee Balance");
         financialPanel.add(balanceButton);
         balanceButton.addActionListener(e -> {
+            FeeBalance feeBalanceWindow = new FeeBalance();
+            feeBalanceWindow.setVisible(true);
         });
 
         hostelsButton = new JButton("Hostels");
         financialPanel.add(hostelsButton);
         hostelsButton.addActionListener(e -> {
-
+            HostelDetails hostelDetailsWindow = new HostelDetails();
+            hostelDetailsWindow.setVisible(true);
         });
 
         add(financialPanel);
@@ -37,4 +40,30 @@ public class Financial extends JFrame {
         setVisible(true);
     }
 
+    // Sample implementation of the FeeStructure class
+    private static class FeeStructure extends JFrame {
+        public FeeStructure() {
+            setTitle("Fee Structure");
+            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setSize(200, 200);
+        }
+    }
+
+    // Sample implementation of the FeeBalance class
+    private static class FeeBalance extends JFrame {
+        public FeeBalance() {
+            setTitle("Fee Balance");
+            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setSize(200, 200);
+        }
+    }
+
+    // Sample implementation of the HostelDetails class
+    private static class HostelDetails extends JFrame {
+        public HostelDetails() {
+            setTitle("Hostel Details");
+            setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            setSize(200, 200);
+        }
+    }
 }
